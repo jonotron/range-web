d67d9f5655f9b363fd9fc1c0b219f005b5c37508
Jonathan Bowers <jonotron@gmail.com>
2019-10-09 10:43:23 -0700

    use the story for tests

--- src/components/rangeUsePlanPage/versioning/ApprovedVersionPreview.test.js
+++ src/components/rangeUsePlanPage/versioning/ApprovedVersionPreview.test.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-
-import ApprovedVersionPreview from './ApprovedVersionPreview'
+import { defaultStory } from './ApprovedVersionPreview.story'
 
 it('should display version, date, and author', () => {
-  const props = {
-    plan: { version: 'v2', date: 'June 30, 2019', author: 'Jane Doe' }
-  }
-  const { container, getByText } = render(<ApprovedVersionPreview {...props} />)
+  const { container, getByText } = render(defaultStory())
 
   expect(getByText('v2')).toBeTruthy()
   expect(getByText('June 30, 2019')).toBeTruthy()
